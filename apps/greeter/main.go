dd44c4af31835d9977b0ce6e599b7c602ac5c1c8
Espen Strømland <espen.stromland@skatteetaten.no>
2020-09-03 14:27:13 +0200

    updated app

--- apps/greeter/main.go
+++ apps/greeter/main.go
@@ -4,13 +4,41 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
+	"strings"
+	"time"
 )
 
 func main() {
+	podName := os.Getenv("NAME")
+	version := os.Getenv("VERSION")
+
+	log.Printf("Application version %s\n", version)
+	log.Println("Starting application")
+
+	// Denne er kun for å demonstrere hvordan Kubernetes håndterer applikasjoner
+	// som ikke klarer å starte.
+	if version == "3" {
+		log.Println("Waiting")
+		time.Sleep(5 * time.Second)
+		log.Fatal("Failed to start application")
+	}
+
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Hello, UiA")
+		paths := strings.Split(r.URL.Path, "/")
+
+		var fromName string
+		if r.URL.Path != "/" && len(paths) >= 2 {
+			fromName = paths[1]
+		} else if podName != "" {
+			fromName = podName
+		} else {
+			fromName = "App"
+		}
+
+		fmt.Fprintf(w, "Hei, Uia! Hilsen %s.\nTime %s\nVersion %s\n", fromName, time.Now().Local(), version)
 	})
 
-	fmt.Println("Starting server on port 8080")
+	log.Println("Starting server on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
